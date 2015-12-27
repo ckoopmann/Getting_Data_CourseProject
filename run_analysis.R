55d18e653aef7dcbd602f43c9a605b435e7bcbf8
ckoopmann <c.k.e.koopmann@gmail.com>
2015-12-27 17:17:12 +0100

    Minor change in R Script

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,15 @@
 
-setwd("/Users/Christian/Statistik_Studium/Online_Kurse/Getting_Data/course_project")
 
-#Download zip-file
+#1. Download zip-file
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 fileDest <- "dataset.zip"
 download.file(fileURL, fileDest, method = 'curl')
 unzip(fileDest)
 
-#Load labelnames
+#2. Load labelnames
 labeldata <- read.table(file = "./UCI HAR Dataset/features.txt" )
 
-#Load and label testdata
+#3. Load and label testdata
 testdata <- read.table(file = "./UCI HAR Dataset/test/X_test.txt")
 names(testdata) <- labeldata[,2]
 testsubjects <- read.table(file = "./UCI HAR Dataset/test/subject_test.txt")
@@ -18,10 +17,10 @@ testactivity <- read.table(file = "./UCI HAR Dataset/test/y_test.txt")
 testdata$subjectid <- testsubjects[,1]
 testdata$activity <- testactivity[,1]
 
-#remove unneeded data to free memory
+# remove unneeded data to free memory
 rm(testsubjects); rm(testactivity)
 
-#Load and label traindata 
+#3. Load and label traindata 
 traindata <- read.table(file = "./UCI HAR Dataset/train/X_train.txt")
 names(traindata) <- labeldata[,2]
 trainsubjects <- read.table(file = "./UCI HAR Dataset/train/subject_train.txt")
@@ -32,33 +31,36 @@ traindata$activity <- trainactivity[,1]
 #remove unneeded data to free memory
 rm(trainsubjects); rm(trainactivity); rm(labeldata)
 
-#merge test and traindata
+#4. merge test and traindata
 data <- rbind(testdata, traindata)
+
 #remove test and traindatasets to free memory
 rm(testdata); rm(traindata)
-#create logical vector to only select subjectid, activity, mean and std values
+
+#5. create logical vector to only select subjectid, activity, mean and std values
 selection <- grep("mean|std|subjectid|activity", names(data))
 reddata <- data[,selection]
 
 #remove unneeded data to free memory
 rm(data); rm(selection)
 
-#Reorder data to get subjectid and activity as first and second column respectively
+#6. Reorder data to get subjectid and activity as first and second column respectively
 reddata <- reddata[,c(80,81,1:79)]
 
-#Labeling activities
+#7. Labeling activities
 activitylabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 reddata$activity <- factor(reddata$activity, labels = tolower(activitylabels[,2]))
 
-#Creating descriptive variable names
+#8. Creating descriptive variable names
 toreplace <- c("Freq", "^t", "^f", "Acc", "Gyro", "Jerk", "Mag", "mean", "std", "\\()", "\\-", "Body", "Gravity")
 replace <- c("Frequency\\.", "TimeDomain\\.", "FrequencyDomain\\.", "Accelerometer\\.", "Gyroscope\\.", "JerkSignal\\.", "Magnitutde\\.", "MeanValue\\.", "StandardDeviation\\.",  "", "", "Body.", "Gravity.")
 for(i in 1:length(replace)){
       names(reddata) <- gsub(pattern = toreplace[i], replacement = replace[i], x = names(reddata))   
 }
 
-#Creating New Dataset with averages for all subjects and activities
+#9. Creating New Dataset with averages for all subjects and activities
 library(dplyr)
 summarised_data <- reddata %>% group_by(subjectid, activity) %>% summarise_each(funs(mean))
 
-#Writing output into file
+#10. Writing output into file
+write.table(summarised_data, file="output.txt", row.names=F)
